a2f3912a496441577dbf422c0d5a905b13becbf3
Avilash2001 <avilashghosh2001@gmail.com>
2021-06-16 15:13:37 +0530

    fixed bug

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGO_URL:
+    "mongodb+srv://adwait:adwait@aptitudeportal-tst.sz6i4.mongodb.net/merngDB?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
