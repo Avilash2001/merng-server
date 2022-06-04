02c3d2897134d1836e1566dab71086708e78a241
Avilash-Fleapo <97432966+Avilash-Fleapo@users.noreply.github.com>
2022-06-04 17:02:55 +0530

    updated string

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGO_URL:
-    "mongodb+srv://adwait:adwait@aptitudeportal-tst.sz6i4.mongodb.net/merngDB?retryWrites=true&w=majority",
+    "mongodb+srv://Avixion:babil2001@cluster0.kgylnql.mongodb.net/?retryWrites=true&w=majority",
   SECRET_KEY: "some very secret key",
 };
